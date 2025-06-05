e8cd0acb1dc06f5461dac4af91a38f2fb72a0db7
LX <132520325+laminsaidy@users.noreply.github.com>
2025-06-06 01:48:20 +0200

    Clean up admin.py and urls.py by resolving merge conflicts

--- todo/admin.py
+++ todo/admin.py
@@ -1,36 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from todo.models import User, Profile  
-
-from .models import Todo  
-
-
-# Customizing the admin interface for Todo model
-class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'status')  
- 
-
-
-# Registering the Todo model with the customized TodoAdmin
-admin.site.register(Todo, TodoAdmin)  
-
-
-# Customizing the admin interface for User model
-class UserAdmin(admin.ModelAdmin):
-    # Display the username and email in the list view of the User model
-    list_display = ['username', 'email']  
-
-
-# Customizing the admin interface for Profile model
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'full_name', 'verified']  
-    list_editable = ['verified', ]  
-
-
-# Registering the User and Profile models with their customized admin interfaces
-admin.site.register(User, UserAdmin)  
-admin.site.register(Profile, ProfileAdmin)  
-=======
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, Profile, Todo
 
@@ -72,4 +40,3 @@ class ProfileAdmin(admin.ModelAdmin):
 
 # Registering the Profile model with the customized ProfileAdmin
 admin.site.register(Profile, ProfileAdmin)
->>>>>>> main
--- todo/urls.py
+++ todo/urls.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-from django.urls import path
-from . import views
-from rest_framework_simplejwt.views import TokenRefreshView
-
-urlpatterns = [
-    path('tasks/summary/', views.task_summary, name='task_summary'),
-    path('tasks/add/', views.add_task, name='add_task'),
-    path('tasks/statistics/', views.view_statistics, name='view_statistics'),
-    path('', views.getRoutes, name='get_routes'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('test/', views.testEndPoint, name='test'),
-=======
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
@@ -23,14 +10,24 @@ router = DefaultRouter()
 router.register(r'todos', views.TodoViewSet, basename='todo')
 
 urlpatterns = [
+    # Authentication endpoints
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('csrf/', views.get_csrf_token, name='csrf'),
-
+    
+    # Todo endpoints
+    path('tasks/summary/', views.task_summary, name='task_summary'),
+    path('tasks/add/', views.add_task, name='add_task'),
+    path('tasks/statistics/', views.view_statistics, name='view_statistics'),
+    
+    # Router endpoints
     path('', include(router.urls)),
+    
+    # Other endpoints
     path('profile/', views.get_user_profile, name='profile'),
->>>>>>> main
+    path('test/', views.testEndPoint, name='test'),
+    path('', views.getRoutes, name='get_routes'),
 ]
