5f907489a86585092a6a569d75795f3fc13e0d68
LX <132520325+laminsaidy@users.noreply.github.com>
2025-08-07 22:06:56 +0200

    Fix NameError by importing dj_database_url and update middleware for production

--- backend/settings.py
+++ backend/settings.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
+import dj_database_url  
 
 load_dotenv()
 
@@ -61,6 +62,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
