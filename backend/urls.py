b428d706f110d43a13ac6324540056e277719359
LX <132520325+laminsaidy@users.noreply.github.com>
2025-08-07 23:05:58 +0200

    Redirect root URL to Django admin login page for improved backend navigation

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,5 @@
-# backend/main urls
-
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
@@ -10,18 +7,15 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView
 )
-
-def home(request):
-    """Simple health check endpoint"""
-    return HttpResponse("Welcome to the API!")
+from django.shortcuts import redirect 
 
 urlpatterns = [
+    path('', lambda request: redirect('admin/', permanent=False)),  
     path('admin/', admin.site.urls),
     path('api/', include('todo.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('', home, name='home'),
 ]
 
 if settings.DEBUG:
